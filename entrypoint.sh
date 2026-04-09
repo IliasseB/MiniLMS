23b9dae57c095cd9073934613fe89bae9bf93f44
IliasseB <iliasse.bellouch@edu.univ-paris13.fr>
2026-04-09 11:17:31 +0200

    fix: remove register button

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,8 @@ set -e
 touch /var/www/html/database/database.sqlite
 chown www-data:www-data /var/www/html/database/database.sqlite
 
+echo "==> Lancement des migrations..."
 php artisan migrate:fresh --seed --force
+echo "==> Migrations terminées"
 
 apache2-foreground
